2b3a6b2e069d2f7c3c856c864e1efb33e94fa181
koorchik <koorchik@gmail.com>
2015-03-24 16:41:43 +0200

    Update version

--- lib/Mojolicious/Plugin/ValidateTiny.pm
+++ lib/Mojolicious/Plugin/ValidateTiny.pm
@@ -8,7 +8,7 @@ use Carp qw/croak/;
 use Validate::Tiny;
 no if $] >= 5.017011, warnings => 'experimental::smartmatch';
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 sub register {
     my ( $self, $app, $conf ) = @_;
@@ -29,7 +29,7 @@ sub register {
                 unless ref($rules) ~~ [ 'ARRAY', 'HASH' ];
 
             $c->stash('validate_tiny.was_called', 1);
-            
+
             $rules = { checks => $rules } if ref $rules eq 'ARRAY';
             $rules->{fields} ||= [];
 
@@ -38,7 +38,7 @@ sub register {
 
             #Latest mojolicious has an issue in that it doesn't include route supplied parameters so we need to hack that in.
             $params = { %{$params},  %{$c->stash->{'mojo.captures'}} };
-            
+
             # Autofill fields
             if ( $conf->{autofields} ) {
                 push @{$rules->{fields}}, keys %$params;
@@ -48,10 +48,10 @@ sub register {
                     push @{$rules->{fields}}, $field;
                 }
             }
-            
+
             # Remove fields duplications
             my %h;
-            @{$rules->{fields}} = grep { !$h{$_}++ } @{$rules->{fields}};    
+            @{$rules->{fields}} = grep { !$h{$_}++ } @{$rules->{fields}};
 
             # Check that there is an individual rule for every field
             if ( $conf->{explicit} ) {
@@ -66,7 +66,7 @@ sub register {
                 if (@fields_wo_rules) {
                     my $err_msg = 'ValidateTiny: No validation rules for '
                         . join( ', ', map { qq'"$_"' } @fields_wo_rules );
-                    
+
                     my $errors = {};
                     foreach my $f (@fields_wo_rules) {
                         $errors->{$f} = "No validation rules for field \"$f\"";
@@ -80,7 +80,7 @@ sub register {
             # Do validation
             my $result = Validate::Tiny->new( $params, $rules );
             $c->stash( 'validate_tiny.result' => $result );
-            
+
             if ( $result->success ) {
                 $log->debug('ValidateTiny: Successful');
                 return $result->data;
@@ -122,37 +122,37 @@ sub register {
             $params //= {};
 
 		    return $c->stash('validate_tiny.result')->error_string(%$params);
-        } );        
+        } );
 
     # Helper validator_any_error
     $app->helper(
         validator_any_error => sub {
             my ( $c ) = @_;
             my $errors = $c->stash('validate_tiny.errors');
-            
+
             if ( $errors ) {
                 return ( ( values %$errors )[0] );
             }
-            
+
             return;
         } );
 
 
-    # Print info about actions without validation    
+    # Print info about actions without validation
     $app->hook(
         after_dispatch => sub {
             my ($c) = @_;
             my $stash = $c->stash;
             return 1 if $stash->{'validate_tiny.was_called'};
-            
+
             if ( $stash->{controller} && $stash->{action} ) {
-                $log->debug("ValidateTiny: No validation in [$stash->{controller}#$stash->{action}]");    
+                $log->debug("ValidateTiny: No validation in [$stash->{controller}#$stash->{action}]");
                 return 0;
             }
-            
+
             return 1;
     } );
-    
+
 }
 
 1;
@@ -165,11 +165,11 @@ Mojolicious::Plugin::ValidateTiny - Lightweight validator for Mojolicious
 
     # Mojolicious
     $self->plugin('ValidateTiny');
-    
+
     # Mojolicious::Lite
     plugin 'ValidateTiny';
-    
-    sub action { 
+
+    sub action {
         my $self = shift;
         my $validate_rules = [
              # All of these are required
@@ -185,15 +185,15 @@ Mojolicious::Plugin::ValidateTiny - Lightweight validator for Mojolicious
              }
         ];
         return unless $self->do_validation($validate_rules);
-        
+
         ... Do something ...
     }
-        
-        
+
+
     sub action2 {
         my $self = shift;
 
-        my $validate_rules = { 
+        my $validate_rules = {
             checks  => [...],
             fields  => [...],
             filters => [...]
@@ -202,19 +202,19 @@ Mojolicious::Plugin::ValidateTiny - Lightweight validator for Mojolicious
             # all $params are validated and filters are applyed
             ... do your action ...
 
-         
+
         } else {
             my $errors     = $self->validator_error;             # hash with errors
             my $pass_error = $self->validator_error('password'); # password error text
             my $any_error  = $self->validator_any_error;         # any error text
-            
-            $self->render( status => '403', text => $any_error );  
+
+            $self->render( status => '403', text => $any_error );
         }
-        
+
     }
-    
+
     __DATA__
-  
+
     @@ user.html.ep
     %= if (validator_has_errors) {
         <div class="error">Please, correct the errors below.</div>
@@ -223,11 +223,11 @@ Mojolicious::Plugin::ValidateTiny - Lightweight validator for Mojolicious
         <label for="username">Username</label><br />
         <%= input_tag 'username' %><br />
         <%= validator_error 'username' %><br />
-  
+
         <%= submit_button %>
     % end
 
-  
+
 =head1 DESCRIPTION
 
 L<Mojolicious::Plugin::ValidateTiny> is a L<Validate::Tiny> support for L<Mojolicious>.
@@ -241,13 +241,13 @@ If "explicit" is true then for every field must be provided check rule
 =head2 C<autofields> (default 1)
 
 If "autofields" then validator will automatically create fields list based on checks passed to validator.
-So, you can pass: 
+So, you can pass:
     [
         user => is_required(),
         pass => is_required(),
     ]
 
-instead of 
+instead of
 
     {
         fields => ['user', 'pass'],
@@ -277,7 +277,7 @@ $VALIDATE_RULES - Validate::Tiny rules in next form
         filters => [],      # Optional
     }
 
-You can pass only "checks" arrayref to "do_validation". 
+You can pass only "checks" arrayref to "do_validation".
 In this case validator will take all GET+POST parameters as "fields"
 
 returns false if validation failed
@@ -316,9 +316,9 @@ Helper maps directly to Validate::Tiny::error_string method ( see L<Validate::Ti
     my $error_str = $self->validator_error_string();
 
     <%= validator_error_string %>
-    
+
 =head2 C<validator_any_error>
-    
+
 Returns any of the existing errors. This method is usefull if you want return only one error.
 
 =head1 AUTHOR
@@ -331,6 +331,6 @@ Please report any bugs or feature requests to Github L<https://github.com/koorch
 
 =head1 SEE ALSO
 
-L<Validate::Tiny>, L<Mojolicious>, L<Mojolicious::Plugin::Validator> 
+L<Validate::Tiny>, L<Mojolicious>, L<Mojolicious::Plugin::Validator>
 
 =cut
